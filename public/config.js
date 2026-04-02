6b8f5eadb69f995a32e408d16ecc952d68d3ee5e
Vidar <33669232+MatPomGit@users.noreply.github.com>
2026-04-02 20:13:37 +0200

    Dodaj konfigurowalne ustawienia grafiki i animacji

---
+++ public/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+window.PSYCHOPOLY_DEFAULT_CONFIG = {
+  animationSpeed: 1, // 0.5 - 2.0
+  fontScale: 1,      // 0.8 - 1.4
+  renderQuality: 'high', // low | medium | high
+  boardFxIntensity: 1,   // 0.5 - 1.5
+};
